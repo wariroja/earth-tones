c6540e967248f2ace4b16f31ca5d7ad85c763c18
Warinya/วริญญา <wariroja@gmail.com>
2023-10-14 16:30:28 -0700

    initial commit

---
+++ earth-tones/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [chords, setChords] = useState('');
+  const [key, setKey] = useState('');
+  const [bpm, setBpm] = useState('');
+  const [music, setMusic] = useState(null);
+
+  const generateMusic = async () => {
+    const response = await fetch('http://localhost:5000/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ chords, key, bpm }),
+    });
+
+    const data = await response.json();
+    setMusic(data.music);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={chords}
+        onChange={(e) => setChords(e.target.value)}
+        placeholder="Chords"
+      />
+      <input
+        type="text"
+        value={key}
+        onChange={(e) => setKey(e.target.value)}
+        placeholder="Key"
+      />
+      <input
+        type="number"
+        value={bpm}
+        onChange={(e) => setBpm(e.target.value)}
+        placeholder="BPM"
+      />
+      <button onClick={generateMusic}>Generate Music</button>
+      {music && <audio src={music} controls />}
+    </div>
+  );
+}
+
+export default App;
